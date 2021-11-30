7dc5fe34d46ada2ad5220f48201b3bf9821ec597
张兴柱 <zhangxz2>
2021-11-30 14:18:36 +0800

    a

--- 第01周   Go语言基础知识/2-Go基础语法/code/hello.go
+++ 第01周   Go语言基础知识/2-Go基础语法/code/hello.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+//变量定义
+func variableZeroValue() {
+	var a int
+	var b string
+	fmt.Println(a, b)
+	fmt.Printf("%d %q\n", a, b) //打印空串标记
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4 //定义多个变量
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
 func main() {
 	fmt.Println("Hello World")
+	variableZeroValue()
+	variableInitialValue()
 }
