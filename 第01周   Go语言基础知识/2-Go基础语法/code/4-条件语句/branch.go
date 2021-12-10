bf135360d4a339daec77368a66777ddb99e0a2e4
张兴柱 <zhangxz2>
2021-12-10 11:30:18 +0800

    分支

--- 第01周   Go语言基础知识/2-Go基础语法/code/4-条件语句/branch.go
+++ 第01周   Go语言基础知识/2-Go基础语法/code/4-条件语句/branch.go
@@ -7,11 +7,55 @@ import (
 
 func main() {
 	const filename = "abc.txt"
-	contents, err := ioutil.ReadFile(filename)
-	if err != nil {
+	// contents, err := ioutil.ReadFile(filename)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Printf("%s\n", contents)
+	// }
+	//更简单的写法
+	if contents, err := ioutil.ReadFile(filename); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Printf("%s\n", contents)
 	}
+	//fmt.Println(contents)  //contents 出了if就不能被访问
+	println(grade(80))
+	println(grade(90))
+	println(grade(101))
 
+}
+
+func eval(a, b int, op string) int {
+	var result int
+	switch op {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "*":
+		result = a * b
+	case "/":
+		result = a / b
+	default:
+		panic("unsurported operator:" + op)
+	}
+	return result
+}
+
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("wrong score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score < 100:
+		g = "A"
+	}
+	return g
 }
