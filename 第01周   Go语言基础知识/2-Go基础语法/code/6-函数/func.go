ae0df810b7446ffceffcb66c716452a89d943d47
张兴柱 <zhangxz2>
2021-12-13 11:51:55 +0800

    func

--- 第01周   Go语言基础知识/2-Go基础语法/code/6-函数/func.go
+++ 第01周   Go语言基础知识/2-Go基础语法/code/6-函数/func.go
@@ -19,8 +19,54 @@ func div3(a, b int) (q, r int) {
 	return
 }
 
+func eval(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		q, _ := div2(13, 3) //多个返回值只使用一个，变量用_
+		return q
+	default:
+		panic("unsurported operator:" + op)
+	}
+}
+
+func eval2(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		q, _ := div2(13, 3) //多个返回值只使用一个，变量用_
+		return q, nil
+	default:
+		return 0, fmt.Errorf("unsurported operator: %s", op)
+	}
+}
+
+//体现函数是一等公民
+func apply(op func(int, int) int, a, b int) int {
+	return op(a, b)
+}
+
 func main() {
 	fmt.Println(div(13, 3))
 	q, r := div2(13, 3)
 	fmt.Println(q, r)
+
+	fmt.Println(eval2(1, 2, "n"))
+
+	if result, err := eval2(1, 2, "n"); err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println(result)
+	}
+
 }
