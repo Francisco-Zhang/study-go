d2c34587efd187bb6b79ffc96277081838b20a49
张兴柱 <zhangxz2>
2021-12-13 14:14:07 +0800

    func

--- 第01周   Go语言基础知识/2-Go基础语法/code/6-函数/func.go
+++ 第01周   Go语言基础知识/2-Go基础语法/code/6-函数/func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
 
 //函数可以返回多个值 	13 /3 = 4 ...1
 func div(a, b int) (int, int) {
@@ -53,7 +58,23 @@ func eval2(a, b int, op string) (int, error) {
 
 //体现函数是一等公民
 func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args "+
+		"(%d,%d)\n", opName, a, b)
 	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func sum(numbers ...int) int {
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
 }
 
 func main() {
@@ -69,4 +90,9 @@ func main() {
 		fmt.Println(result)
 	}
 
+	fmt.Println(apply(func(a, b int) int {
+		return int(math.Pow(float64(a), float64(b)))
+	}, 3, 4))
+
+	fmt.Println(sum(1, 2, 3))
 }
