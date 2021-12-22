de8798435a9ee3bafcacf7cf12a82e0e43bc7f70
张兴柱 <zhangxz2>
2021-12-22 15:23:07 +0800

    map

--- 第01周   Go语言基础知识/3-内建容器/code/3-Map/maps.go
+++ 第01周   Go语言基础知识/3-内建容器/code/3-Map/maps.go
@@ -9,7 +9,29 @@ func main() {
 		"site":    "imooc",
 		"quality": "notbad",
 	}
-	m2 := make(map[string]int)
+	m2 := make(map[string]int) //m2 == empty map
+	var m3 map[string]int      //m3 == nill
 
-	fmt.Println(m, m2)
+	fmt.Println(m, m2, m3)
+
+	fmt.Println("Traversing map")
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Getting values")
+	courseName, ok := m["course"] //key不存在，返回空串,ok==false
+	fmt.Println(courseName, ok)
+	if cname, ok := m["cname"]; ok {
+		fmt.Println(cname)
+	} else {
+		fmt.Println("key does not exist")
+	}
+
+	fmt.Println("Deleting values")
+	delete(m, "name")
+	name, ok := m["name"]
+	fmt.Println(name, ok)
+
 }
