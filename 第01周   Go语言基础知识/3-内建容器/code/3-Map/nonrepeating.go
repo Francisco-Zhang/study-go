16f92191876a69e61b61545faba93e429bc48088
张兴柱 <zhangxz2>
2021-12-24 09:59:09 +0800

    rune

--- 第01周   Go语言基础知识/3-内建容器/code/3-Map/nonrepeating.go
+++ 第01周   Go语言基础知识/3-内建容器/code/3-Map/nonrepeating.go
@@ -20,8 +20,26 @@ func lengthOfNonRepeatingSubStr(s string) int {
 	return maxLength
 }
 
+//支持中文
+func lengthOfNonRepeatingSubStr2(s string) int {
+	lastOccurred := make(map[rune]int)
+	start := 0
+	maxLength := 0
+	for i, ch := range []rune(s) {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastOccurred[ch] + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+	return maxLength
+}
+
 func main() {
 	fmt.Println(lengthOfNonRepeatingSubStr("abcabcbb"))
 	fmt.Println(lengthOfNonRepeatingSubStr("bbbb"))
 	fmt.Println(lengthOfNonRepeatingSubStr("pwwkew"))
+	fmt.Println(lengthOfNonRepeatingSubStr2("一二三三二一"))
 }
