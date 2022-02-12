f64b3cce3007eefb48b791428f3567465571fdc6
张兴柱 <zhangxz2>
2022-02-12 11:00:23 +0800

    validator

---
+++ 第20周   用户服务的web服务/2、web层开发-用户接口开发/code/2、 自定义mobile验证器/mxshop-api/user-web/api/user.go
@@ -0,0 +1,124 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"mxshop-api/user-web/forms"
+	"mxshop-api/user-web/global"
+	"mxshop-api/user-web/global/reponse"
+	"mxshop-api/user-web/proto"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func removeTopStruct(fileds map[string]string) map[string]string {
+	rsp := map[string]string{}
+	for field, err := range fileds {
+		rsp[field[strings.Index(field, ".")+1:]] = err
+	}
+	return rsp
+}
+
+func HandleGrpcErrorToHttp(err error, c *gin.Context) {
+	//将grpc的code转换成http的状态码
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"msg": e.Message(),
+				})
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg:": "内部错误",
+				})
+			case codes.InvalidArgument:
+				c.JSON(http.StatusBadRequest, gin.H{
+					"msg": "参数错误",
+				})
+			case codes.Unavailable:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "用户服务不可用",
+				})
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": e.Code(),
+				})
+			}
+			return
+		}
+	}
+}
+
+func HandleValidatorError(c *gin.Context, err error) {
+	errs, ok := err.(validator.ValidationErrors)
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": err.Error(),
+		})
+	}
+	c.JSON(http.StatusBadRequest, gin.H{
+		"error": removeTopStruct(errs.Translate(global.Trans)),
+	})
+	return
+}
+
+func GetUserList(ctx *gin.Context) {
+
+	//拨号连接用户grpc服务器
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", global.ServerConfig.UserSrvInfo.Host, global.ServerConfig.UserSrvInfo.Port), grpc.WithInsecure())
+	if err != nil {
+		zap.S().Errorw("[GetUserList] 连接 【用户服务】失败",
+			"msg", err.Error())
+	}
+
+	pn := ctx.DefaultQuery("pn", "0")
+	pnInt, _ := strconv.Atoi(pn)
+	pSize := ctx.DefaultQuery("psize", "10")
+	pSizeInt, _ := strconv.Atoi(pSize)
+
+	//生成grpc的client，并调用接口
+	userSrvClient := proto.NewUserClient(conn)
+	rsp, err := userSrvClient.GetUserList(context.Background(), &proto.PageInfo{
+		Pn:    uint32(pnInt),
+		PSize: uint32(pSizeInt),
+	})
+	if err != nil {
+		zap.S().Errorw("[GetUserList] 查询 【用户列表】失败",
+			"msg", err.Error())
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		user := reponse.UserResponse{
+			Id:       value.Id,
+			NickName: value.NickName,
+			//Birthday: time.Time(time.Unix(int64(value.BirthDay), 0)).Format("2006-01-02"),
+			Birthday: reponse.JsonTime(time.Unix(int64(value.BirthDay), 0)),
+			Gender:   value.Gender,
+			Mobile:   value.Mobile,
+		}
+		result = append(result, user)
+	}
+	ctx.JSON(http.StatusOK, result)
+	zap.S().Debug("获取用户列表页")
+}
+
+func PassWordLogin(c *gin.Context) {
+	//表单验证
+	passwordLoginForm := forms.PassWordLoginForm{}
+	if err := c.ShouldBind(&passwordLoginForm); err != nil {
+		HandleValidatorError(c, err)
+		return
+	}
+
+}
--- 第20周   用户服务的web服务/2、web层开发-用户接口开发/doc/web层开发-用户接口开发.md
+++ 第20周   用户服务的web服务/2、web层开发-用户接口开发/doc/web层开发-用户接口开发.md
@@ -1,1 +1,33 @@
 ## 1 、表单验证的初始化
+
+## 2、 自定义mobile验证器
+
+```go
+type PassWordLoginForm struct {
+	Mobile   string `form:"mobile" json:"mobile" binding:"required"` //手机号码格式有规范可寻， 自定义validator
+	PassWord string `form:"password" json:"password" binding:"required,min=3,max=20"`
+}
+
+
+func ValidateMobile(fl validator.FieldLevel) bool {
+	mobile := fl.Field().String()
+	//使用正则表达式判断是否合法
+	ok, _ := regexp.MatchString(`^1([38][0-9]|14[579]|5[^4]|16[6]|7[1-35-8]|9[189])\d{8}$`, mobile)
+	if !ok{
+		return false
+	}
+	return true
+}
+
+//注册验证器
+if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+    _ = v.RegisterValidation("mobile", myvalidator.ValidateMobile)
+    _ = v.RegisterTranslation("mobile", global.Trans, func(ut ut.Translator) error {
+        return ut.Add("mobile", "{0} 非法的手机号码!", true) // see universal-translator for details
+    }, func(ut ut.Translator, fe validator.FieldError) string {
+        t, _ := ut.T("mobile", fe.Field())
+        return t
+    })
+}
+```
+
