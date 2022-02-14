d27f4445183e40ba7f17fb17dbf1d7861ee66d2a
张兴柱 <zhangxz2>
2022-02-14 11:24:05 +0800

    gin grpc consul

---
+++ 第21周   服务注册发现、配置中心、负载均衡/2、负载均衡/code/mxshop-api/user-web/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin/binding"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"mxshop-api/user-web/global"
+	"mxshop-api/user-web/initialize"
+	"mxshop-api/user-web/utils"
+	myvalidator "mxshop-api/user-web/validator"
+)
+
+func main() {
+	//1. 初始化logger
+	initialize.InitLogger()
+	//2. 初始化配置文件
+	initialize.InitConfig()
+
+	//3. 初始化routers
+	Router := initialize.Routers()
+
+	//4. 初始化翻译
+	if err := initialize.InitTrans("zh"); err != nil {
+		panic(err)
+	}
+
+	//5. 初始化srv的连接
+	initialize.InitSrvConn()
+
+	viper.AutomaticEnv()
+	//如果是本地开发环境端口号固定，线上环境启动获取端口号
+	debug := viper.GetBool("MXSHOP_DEBUG")
+	if !debug {
+		port, err := utils.GetFreePort()
+		if err == nil {
+			global.ServerConfig.Port = port
+		}
+	}
+
+	//注册验证器
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		_ = v.RegisterValidation("mobile", myvalidator.ValidateMobile)
+		_ = v.RegisterTranslation("mobile", global.Trans, func(ut ut.Translator) error {
+			return ut.Add("mobile", "{0} 非法的手机号码!", true) // see universal-translator for details
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("mobile", fe.Field())
+			return t
+		})
+	}
+
+	//使用全局的log,缩短代码
+	/*
+		1. S()可以获取一个全局的sugar，这个sugar什么都没做，需要我们自己设置一个全局的logger
+			logger, _ := zap.NewDevelopment()
+			zap.ReplaceGlobals(logger)
+		2. 日志是分级别的，debug， info ， warn， error， fetal,生成环境不打印debug
+		3. S函数和L函数很有用， 提供了一个全局的安全访问logger的途径
+	*/
+	zap.S().Infof("启动服务器,端口：%d", global.ServerConfig.Port)
+
+	err := Router.Run(fmt.Sprintf(":%d", global.ServerConfig.Port))
+	if err != nil {
+		zap.S().Panic("启动失败", err.Error())
+	}
+}
