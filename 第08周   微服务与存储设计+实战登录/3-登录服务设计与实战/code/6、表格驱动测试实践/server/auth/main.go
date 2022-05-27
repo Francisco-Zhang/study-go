7d17fdb29d3eacb1624b117e427f8a70efda0131
张兴柱 <mr.francisco@foxmail.com>
2022-05-28 00:03:50 +0800

    a

---
+++ 第08周   微服务与存储设计+实战登录/3-登录服务设计与实战/code/6、表格驱动测试实践/server/auth/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	authpb "coolcar/auth/api/gen/v1"
+	"coolcar/auth/auth"
+	"coolcar/auth/auth/wechat"
+	"coolcar/auth/dao"
+	"log"
+	"net"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	logger, err := newZapLogger()
+	if err != nil {
+		log.Fatalf("cannot create logger: %v", err)
+	}
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		//输出完日志后，程序自动退出
+		logger.Fatal("cannot  listen:", zap.Error(err))
+	}
+	c := context.Background()
+	mc, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://localhost:27017/coolcar?readPreference=primary&ssl=false"))
+	if err != nil {
+		log.Fatalf("cannot connect mongodb", zap.Error(err))
+	}
+
+	s := grpc.NewServer()
+	//服务里是 *Service，这里就要取地址
+	authpb.RegisterAuthServiceServer(s, &auth.Service{
+		OpenIDResolver: &wechat.Service{
+			AppID:     "wx692887841c1ef470",
+			AppSecret: "571ff2da753b8ca0f2985714dd04b039",
+		}, // *Service 才有login，此处取地址
+		Mongo:  dao.NewMongo(mc.Database("coolcar")),
+		Logger: logger,
+	})
+	err = s.Serve(lis)
+	logger.Fatal("cannot serve:", zap.Error(err))
+}
+
+func newZapLogger() (*zap.Logger, error) {
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.TimeKey = ""
+	return cfg.Build()
+}
